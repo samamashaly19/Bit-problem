f3073445caebed1925715659029fb340c225fb8f
Sama Wesam Mashaly <121673484+samamashaly19@users.noreply.github.com>
2023-10-18 19:29:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n=int.Parse(Console.ReadLine());
+            int x = 0;
+            string s1;
+            string s2 = "++x";
+            string s3 = "x++";
+            string s4 = "--x";
+            string s5 = "x--";
+            for(int i = 0; i < n; i++)
+            { 
+                s1 = Console.ReadLine();
+                if (s1 == s2 || s1 == s3)
+                    x = x + 1;
+                else
+                    x = x - 1;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
